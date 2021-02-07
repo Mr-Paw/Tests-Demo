0bdfe31aad04d179867c25bd67187cfea65d3520
Paul <paw@siteyou.ru>
2021-02-07 13:13:06 +0300

    password validation method changed

--- Tests Demo/Validater.swift
+++ Tests Demo/Validater.swift
@@ -10,6 +10,8 @@ import Foundation
 public class Validater{
     public static func validateEmail(_ email: String) -> Bool{
         var array = email.split(separator: "@")
+        var haveSpecialSym = false
+
         if let lastEl = array.last, array.count != 1{
             array.remove(at: 1)
             for i in lastEl.split(separator: "."){
@@ -22,24 +24,7 @@ public class Validater{
         
     }
     public static func validatePassword(_ password: String) -> Bool{
-        if password.count >= 6  {
-            let strArray = password.map{$0}
-            var haveNumber = false
-            var haveLowercased = false
-            var haveUppercased = false
-            strArray.forEach { (sym) in
-                if sym.isLowercase{
-                    haveLowercased = true
-                }else if sym.isUppercase{
-                    haveUppercased = true
-                }else if sym.isNumber{
-                    haveNumber = true
-                }
-            }
-            
-            return haveNumber && haveLowercased && haveUppercased
-        }else{
-            return false
-        }
+        let predicate = NSPredicate(format: "SELF MATCHES %@ ", "^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{6,}$")
+       return predicate.evaluate(with: password)
     }
 }
