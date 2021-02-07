94bc4488da4413fe1012393a4355e8ecc0759130
Paul <paw@siteyou.ru>
2021-02-07 13:18:17 +0300

    email validation changed
    (cherry picked from commit fc5590791472d63634aaa332bea0eb9ca8868096)

--- Tests Demo/Validater.swift
+++ Tests Demo/Validater.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class Validater{
     public static func validateEmail(_ email: String) -> Bool{
-        let predicate = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+        let predicate = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
         return predicate.evaluate(with: email)
     }
     public static func validatePassword(_ password: String) -> Bool{
